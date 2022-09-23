ec1d52fb982d030e0b3a2c822dcf1bc197b79126
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-24 00:06:08 +1000

    Added Task 15 solution

---
+++ Task_15_HW/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите номер дня недели (от 1 до 7): ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 | day == 7)
+{
+    System.Console.WriteLine("Этот день - выходной!");
+}
+if (day != 6 && day != 7)
+{
+    System.Console.WriteLine("Это будний день!");
+}

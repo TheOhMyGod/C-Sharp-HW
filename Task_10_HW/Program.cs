cd2eea306bf3a711bf4cad3a77eb4bd168a7ba19
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-23 23:35:42 +1000

    Added Task 10 solution

---
+++ Task_10_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int FirstNumber = number / 100;
+int SecondNumber = number / 10 - FirstNumber * 10;
+
+if (number >= 100 && number <= 999) 
+{
+    Console.WriteLine($"Вторая цифра введенного числа - {SecondNumber}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}

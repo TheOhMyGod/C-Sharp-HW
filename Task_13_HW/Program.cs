4dab29c6d23f2e88c156578959d03110c3c1d80a
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-24 00:21:54 +1000

    Added Task 13 solution

---
+++ Task_13_HW/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int reverse = 0;
+
+ while(number>0)  
+            {  
+                int remainder = number % 10;  
+                reverse = (reverse * 10) + remainder;  
+                number = number / 10;  
+            }
+
+ if (reverse >= 100)
+{
+    reverse /= 100;
+    int ThirdNumber = reverse % 10;
+    Console.WriteLine($"Третья цифра введенного числа - {ThirdNumber}");
+}
+
+else Console.WriteLine("Введенное число не имеет третьей цифры!");

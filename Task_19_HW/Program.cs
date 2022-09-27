1999f46fcb9a0f3031ba1e77e53c12a24877adda
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-27 20:55:10 +1000

    Added Task 19 solution

---
+++ Task_19_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите любое пятичзначное число: ");
+
+int InputNumber = int.Parse(Console.ReadLine());
+
+int x = InputNumber / 1000;
+int y = InputNumber % 100;
+
+int FirstDigit = InputNumber / 10000;
+int SecondDigit = x % 10;
+int ForthDigit = y / 10;
+int FifthDigit = InputNumber % 10;
+
+if (FirstDigit == FifthDigit && SecondDigit == ForthDigit)
+{
+    Console.WriteLine($"Число {InputNumber} является палиндромом!");
+}
+else Console.WriteLine($"Число {InputNumber} не является палиндромом!");

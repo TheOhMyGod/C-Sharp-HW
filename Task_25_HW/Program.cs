809d0eef2bc289b4ba8935f390124429e71d806d
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-30 23:02:57 +1000

    Added Task 25 solution

---
+++ Task_25_HW/Program.cs
@@ -0,0 +1,15 @@
+﻿double PowerMethod (int a, int b)
+{
+    double pow = Math.Pow(a,b);
+    return pow;
+}
+
+Console.Write("Введите число, которое хотите возвести в степень: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine());
+
+
+double pow = PowerMethod(a,b);
+Console.Write($"Число {a} в степени {b} равняется {pow}!");

3fe5498e8289ff78c4453c0eb753c1103b8d8202
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-30 23:55:29 +1000

    Added Task 29 solution

---
+++ Task_29_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите элементы массива: ");
+
+int[] array = new int[8];
+int arrLength = array.Length;
+int i = 0;
+
+while (i < arrLength)
+{
+    array[i] = int.Parse(Console.ReadLine()); ;
+    i++;
+}
+
+Console.WriteLine("Ваш массив: ");
+for (int j = 0; j < arrLength; j++)
+{
+    Console.Write($"{array[j]} ");
+}

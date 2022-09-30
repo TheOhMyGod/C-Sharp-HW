62b0d777610d44d2e51eff0b151af943217b1726
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-30 23:40:36 +1000

    Added Task 27 solution

---
+++ Task_27_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+int tmp = number;
+
+while (number > 0)
+{
+    int remainder = number % 10;
+    sum = sum + remainder;
+    number = number / 10;
+}
+
+Console.WriteLine($"Сумма цифр числа {tmp} равна {sum}!");

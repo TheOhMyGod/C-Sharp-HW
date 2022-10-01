a0443c4f4428638048a522eb8eab163d7a986c55
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-02 01:25:25 +1000

    Added Task 34 solution

---
+++ Task_34_HW/Program.cs
@@ -0,0 +1,50 @@
+﻿// Заполняет массив случайными трехзначными числами
+int[] GetArrayFilled (int [] array)
+{
+    var random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+
+    return array;
+}
+
+// Выводит массив
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+}
+
+// Считает четные числа массива
+int GetEvenNumsQuantity (int[] array)
+{
+    int counter = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            counter++;
+        }
+    }
+    
+    return counter;
+}
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] arr = new int[size];
+
+int[] arrayFilled = GetArrayFilled(arr);
+PrintArray(arrayFilled);
+Console.WriteLine();
+
+Console.WriteLine($"Колличество четных чисел в полученом массиве равно {GetEvenNumsQuantity(arrayFilled)}");

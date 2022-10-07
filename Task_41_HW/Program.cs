32d04ad49dcbd1109154d515289c3e79f78a4272
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 15:50:45 +1000

    Added Task 41 solution

---
+++ Task_41_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите любое количество чисел через пробел: ");
+double[] array = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Вы ввели {count} чисел больше нуля!");

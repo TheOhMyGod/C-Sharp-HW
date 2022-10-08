02eba94f93c138bba452f406574520ad42654a1f
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-08 23:29:47 +1000

    Added Task 68 solution

---
+++ Task_68_HW/Program.cs
@@ -0,0 +1,25 @@
+﻿int Ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ack(m - 1, 1);
+    }
+    else
+    {
+        return Ack(m - 1, Ack(m, n - 1));
+    }
+}
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int Ackermann = Ack(m,n);
+
+Console.WriteLine($"Функция Аккермана для чисел ({m},{n}) равна {Ackermann}");

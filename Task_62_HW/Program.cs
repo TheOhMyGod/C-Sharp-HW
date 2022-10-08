3624ecdde9b419a30893c524e8c97095cc1710ca
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-08 22:49:09 +1000

    Added Task 62 solution

---
+++ Task_62_HW/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+            {
+                Console.Write($" {array[i, j]} ");
+            }
+            else
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = 4;
+int[,] array = new int[n, n];
+
+int tmp = 1;
+int i = 0;
+int j = 0;
+
+while (tmp <= array.GetLength(0) * array.GetLength(1))
+{
+    array[i, j] = tmp;
+    tmp++;
+    if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= array.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > array.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+FillArray(array);

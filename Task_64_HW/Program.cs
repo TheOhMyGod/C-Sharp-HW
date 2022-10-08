1e6dac1c18b239e2a5f5726afd3b5bbfd74e3f8f
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-08 22:57:59 +1000

    Added Task 64 solution

---
+++ Task_64_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿void Numbers(int n)
+{
+    if (n > 1)
+    {
+        Console.Write($"{n}, ");
+        Numbers(n - 1);
+    }
+    else if (n == 1)
+    {
+        Console.Write($"{n}");
+    }
+}
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Numbers(n);

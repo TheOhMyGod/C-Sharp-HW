fb70185a0e43f7bda682f092815f32a3e57a9a0d
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-08 23:20:13 +1000

    Added Task 66 solution

---
+++ Task_66_HW/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+int GetSum(int m, int n)
+{
+    if (m < n)
+    {
+        sum += m;
+        GetSum(m + 1, n);
+        return sum + n;
+    }
+    else if (m > n)
+    {
+        sum += n;
+        GetSum(m, n + 1);
+        return sum + m;
+    }
+    else return 0;
+}
+
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {GetSum(m, n)}");
